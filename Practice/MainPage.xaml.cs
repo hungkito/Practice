07533d9d26bca371d3e7cf6a50fe44b45f74efeb
hungkito <hungpvth2008007@fpt.edu.vn>
2021-10-18 14:20:40 +0700

    Add project files.

---
+++ Practice/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Practice
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Icons> Icons
+            ;
+
+        private ObservableCollection<Contacts> Contacts;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Icons = new List<Icons>();
+            Icons.Add(new Icons { IconsPath = "Assets/Lipstick.png" });
+            Icons.Add(new Icons { IconsPath = "Assets/perfume.png" });
+            Icons.Add(new Icons { IconsPath = "Assets/Clock.png" });
+
+            Contacts = new ObservableCollection<Contacts>();
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            string avatar = ((Icons)AvatarComboBox.SelectedValue).IconsPath;
+            Contacts.Add(new Contacts { Product = ProductTextBox.Text, Description = DescriptionTextBox.Text, Image = avatar });
+            ProductTextBox.Text = "";
+            DescriptionTextBox.Text = "";
+            AvatarComboBox.SelectedIndex = -1;
+
+            ProductTextBox.Focus(FocusState.Programmatic);
+        }
+    }
+}
---
+++ Practice/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Models
+{
+    public class Contacts
+    {
+        public string Product { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class Icons
+    {
+        public string IconsPath { get; set; }
+    }
+}
